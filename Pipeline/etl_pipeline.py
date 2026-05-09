4f6a409b8c511421fd8df7c276def290f1a37383
Shaheer Mustafa <115090823+Shaheermustafa2@users.noreply.github.com>
2026-05-09 14:17:04 +0530

    Update etl_pipeline.py

--- Pipeline/etl_pipeline.py
+++ Pipeline/etl_pipeline.py
@@ -2,8 +2,7 @@ import pandas as pd
 import psycopg2
 from psycopg2.extras import execute_values
 
-# ----------DATABASE CONNECTION----------
-
+# ----------Database connection----------
 conn = psycopg2.connect(
     host="localhost",
     database="star_schema_data_warehouse",
@@ -13,77 +12,37 @@ conn = psycopg2.connect(
 
 cursor = conn.cursor()
 
+# ----------loading----------
 def load_table(df, table_name):
-
     query = f"""
-    INSERT INTO {table_name}
-    VALUES %s
-    ON CONFLICT DO NOTHING
+        INSERT INTO {table_name}
+        VALUES %s
+        ON CONFLICT DO NOTHING
     """
 
-    execute_values(
-        cursor,
-        query,
-        df.values.tolist()
-    )
+    execute_values(cursor, query, df.values.tolist())
+    print(f"{table_name} loaded")
 
-    print(f"{table_name} loaded.")
+suppliers = pd.read_csv("data/suppliers.csv")
+warehouses = pd.read_csv("data/warehouses.csv")
+products = pd.read_csv("data/products.csv")
 
-suppliers = pd.read_csv(
-    "data/suppliers.csv"
-)
+load_table(suppliers, "dim_supplier")
+load_table(warehouses, "dim_warehouse")
+load_table(products, "dim_product")
 
-warehouses = pd.read_csv(
-    "data/warehouses.csv"
-)
+shipments = pd.read_csv("data/shipments.csv")
 
-products = pd.read_csv(
-    "data/products.csv"
-)
+shipments["order_date"] = pd.to_datetime(shipments["order_date"])
 
-load_table(
-    suppliers,
-    "dim_supplier"
-)
+dates = shipments[["order_date"]].drop_duplicates()
+dates.columns = ["full_date"]
 
-load_table(
-    warehouses,
-    "dim_warehouse"
-)
+dates["month"] = dates["full_date"].dt.month
+dates["quarter"] = dates["full_date"].dt.quarter
+dates["year"] = dates["full_date"].dt.year
 
-load_table(
-    products,
-    "dim_product"
-)
-
-
-shipments = pd.read_csv(
-    "data/shipments.csv"
-)
-
-dates = pd.DataFrame({
-    "full_date": pd.to_datetime(
-        shipments["order_date"]
-    ).drop_duplicates()
-})
-
-dates["month"] = dates[
-    "full_date"
-].dt.month
-
-dates["quarter"] = dates[
-    "full_date"
-].dt.quarter
-
-dates["year"] = dates[
-    "full_date"
-].dt.year
-
-load_table(
-    dates,
-    "dim_date"
-)
-
+load_table(dates, "dim_date")
 
 shipments["estimated_delivery_date"] = pd.to_datetime(
     shipments["estimated_delivery_date"]
@@ -93,16 +52,10 @@ shipments["actual_delivery_date"] = pd.to_datetime(
     shipments["actual_delivery_date"]
 )
 
-shipments["order_date"] = pd.to_datetime(
-    shipments["order_date"]
-)
-
 shipments["delay_days"] = (
     shipments["actual_delivery_date"]
-    -
-    shipments["estimated_delivery_date"]
+    - shipments["estimated_delivery_date"]
 ).dt.days
-
 
 fact_shipments = shipments[
     [
@@ -118,29 +71,15 @@ fact_shipments = shipments[
     ]
 ]
 
-load_table(
-    fact_shipments,
-    "fact_shipments"
-)
+load_table(fact_shipments, "fact_shipments")
 
-cursor.execute(
-    """
-    SELECT COUNT(*)
-    FROM fact_shipments
-    """
-)
-
+cursor.execute("SELECT COUNT(*) FROM fact_shipments")
 count = cursor.fetchone()[0]
 
-print(
-    f"Fact rows loaded: {count}"
-)
+print(f"Fact rows loaded: {count}")
 
 conn.commit()
-
 cursor.close()
 conn.close()
 
-print(
-    "ETL successful."
-)
+print("ETL completedd")
